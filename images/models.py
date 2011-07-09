6f4359d649fca4413084fcb0e0d7bc741415fe43
Stephen Chan <stephenjchan@gmail.com>
2011-07-08 22:02:38 -0700

    - The New Source form now has more fields and some JavaScript to enhance the form. - base.html template updated with a JavaScript block.
    *Testing notes*
    To update one's copy of the DB accordingly to see the Source changes, one can do something like this (there might be better ways):
        - python manage.py dbshell
        - DROP TABLE coralnet.images_source;
        - (exit the dbshell)
        - python manage.py syncdb
    There's currently no support for viewing this DB table's contents except for using the manage.py shell:
        - python manage.py shell
        - from images.models import Source
        - Source.objects.all()

--- images/models.py
+++ images/models.py
@@ -9,12 +9,47 @@ class Source(models.Model):
     # 'public' = images are visible to the public by default
     # 'private' = images are visible only to the source participators by default
     # 'invisible' = private images + the project's existence is not known to the public
-    visibility = models.CharField(max_length=20)
+    visibility = models.CharField(max_length=1)
 
-    create_date = models.DateTimeField()
+    # Allow us to go from 'b' to 'public' or vice versa
+    visibilityCodeToStr = {'b':'public', 'v':'private', 'i':'invisible'}
+    visibilityStrToCode = dict( [(b,a) for (a,b) in visibilityCodeToStr.items()] )
+
+    create_date = models.DateTimeField('date created')
+
+    # Each of these fields is allowed to be blank (an empty string).
+    # We're assuming that we'll only have key 2 if we have
+    # key 1, we'll only have key 3 if we have key 2, etc.
+    key1 = models.CharField('location key 1', max_length=50, blank=True)
+    key2 = models.CharField('location key 2', max_length=50, blank=True)
+    key3 = models.CharField('location key 3', max_length=50, blank=True)
+    key4 = models.CharField('location key 4', max_length=50, blank=True)
+    key5 = models.CharField('location key 5', max_length=50, blank=True)
+
+    longitude = models.CharField(max_length=20, blank=True)
+    latitude = models.CharField(max_length=20, blank=True)
+
+    def visibilityStr(self):
+        return self.visibilityCodeToStr[self.visibility]
 
     # To-string method
     def __unicode__(self):
-        return "Name: " + self.name \
-               + "\nVisibility: " + self.visibility \
+        displayStr = "\nName: " + self.name \
+               + "\nVisibility: " + self.visibilityStr() \
                + "\nDate created: " + str(self.create_date)
+        if self.key1:
+            displayStr += ("\nLocation keys: " + self.key1)
+        if self.key2:
+            displayStr += (", " + self.key2)
+        if self.key3:
+            displayStr += (", " + self.key3)
+        if self.key4:
+            displayStr += (", " + self.key4)
+        if self.key5:
+            displayStr += (", " + self.key5)
+
+        if self.longitude and self.latitude:
+            displayStr += ("\nWorld location: Longitude " + self.longitude + ", Latitude " + self.latitude)
+
+        displayStr += "\n"
+        return displayStr
--- images/views.py
+++ images/views.py
@@ -18,19 +18,46 @@ def newsource(request):
 def newsource_success(request):
     errorMessages = []
     sourceName = ''
-    visibilitySetting = ''
+    visibilityStr = ''
     
     try:
-        sourceName = request.GET['sourceName']
+        sourceName = request.GET['sourceName'].strip()
+        # After stripping leading/trailing spaces, must be nonempty
         if len(sourceName) == 0:
             raise ValueError
     except (KeyError, ValueError):
         errorMessages.append("Please enter a Source name.")
 
     try:
-        visibilitySetting = request.GET['visibilitySetting']
+        visibilityStr = request.GET['visibilitySetting']
     except KeyError:
         errorMessages.append("Please select a visibility setting.")
+
+    # Location keys.
+    keys = {}
+    for keyNum in ['key1', 'key2', 'key3', 'key4', 'key5']:
+        try:
+            key = request.GET[keyNum].strip()
+            # After stripping leading/trailing spaces, must be nonempty
+            if len(key) == 0:
+                raise ValueError
+            else:
+                keys[keyNum] = key
+
+        except (KeyError, ValueError):
+            break    # Blank key field, so we're done taking keys.
+
+    # Longitude, Latitude.
+    try:
+        longitude = request.GET['longitude'].strip()
+    except KeyError:
+        longitude = None
+
+    try:
+        latitude = request.GET['latitude'].strip()
+    except KeyError:
+        latitude = None
+
 
     if len(errorMessages) > 0:
         # Go back to New Source form.
@@ -40,7 +67,10 @@ def newsource_success(request):
         })
 
     s = Source(name=sourceName,
-               visibility=visibilitySetting,
-               create_date=datetime.datetime.now())
+               visibility=Source.visibilityStrToCode[visibilityStr],
+               create_date=datetime.datetime.now(),
+               longitude=longitude,
+               latitude=latitude,
+               **keys)
     s.save()
     return render_to_response('images/newsource_success.html')
