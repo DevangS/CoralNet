51006e749679b1fa2c4b9f169378c2556176a631
Devang Sampat <devangs27@gmail.com>
2011-07-14 12:29:20 -0700

    Created new models for annotation, images, and groups

--- accounts/models.py
+++ accounts/models.py
@@ -4,8 +4,14 @@ from django.utils.translation import ugettext_lazy as _
 from userena.models import UserenaLanguageBaseProfile
 
 class Profile(UserenaLanguageBaseProfile):
-    first_name = models.CharField(_('first name'), max_length=255, blank=True)
-    last_name = models.CharField(_('last name'), max_length=255, blank=True)
-    website = models.URLField(_('website'), blank=True, verify_exists=True)
-    location =  models.CharField(_('location'), max_length=255, blank=True)
-    about_me = models.TextField(_('about me'), blank=True)
+    first_name = models.CharField(_('First Name'), max_length=45, blank=True)
+    last_name = models.CharField(_('Last Name'), max_length=45, blank=True)
+    website = models.URLField(_('Website'), blank=True, verify_exists=True)
+    location =  models.CharField(_('Location'), max_length=45, blank=True)
+    about_me = models.TextField(_('About Me'), blank=True)
+  #  group = models.ForeignKey(Group)
+
+#class Group(models.Model):
+ #   name = models.CharField(max_length=45, blank=True)
+  #  member_permission = choices("View", "Annotate", "None") #Default permissions for GROUP MEMBERS on GROUPS images
+  #  source_permission = choices("View", "Annotate", "None") #Default permissions for EVERYONE ELSE on GROUPS images
--- annotations/models.py
+++ annotations/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+import accounts
+import images
 
-# Create your models here.
+class annotation_attempt(models.Model):
+    description = models.TextField(blank=True)
+    source = models.ForeignKey(images.models.Source)
+    user = models.ForeignKey(accounts.models.Profile)
+
+class Label(models.Model):
+    name = models.CharField(max_length=45, blank=True)
+    code = models.CharField(max_length=5, blank=True)
+    group = models.ForeignKey(LabelGroup)
+
+class LabelSet(models.Model):
+    description = models.TextField(blank=True)
+    location = models.CharField(max_length=45, blank=True)
+    attempt = models.ForeignKey(annotation_attempt)
+    
+class LabelGroup(models.Model):
+    name = models.CharField(max_length=45, blank=True)
+    code = models.CharField(max_length=5, blank=True)
+
+class Annotation(models.Model):
+    annotation_date = models.DateField()
+    point = models.ForeignKey(images.models.Point)
+    image = models.ForeignKey(images.models.Image)
+    user = models.ForeignKey(accounts.models.Profile)
+    label = models.ForeignKey(Label) #TODO: verify
+    attempt = models.ForeignKey(annotation_attempt)
