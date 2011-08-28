ba8b0656ccac9038646e0ba0502d95dd2948606e
Devang Sampat <devangs27@gmail.com>
2011-08-28 13:55:15 -0700

    Added the year as an option to search against for images

--- visualization/forms.py
+++ visualization/forms.py
@@ -1,12 +1,20 @@
 from django import forms
+from django.forms.fields import DateField
 from django.shortcuts import get_object_or_404
 from annotations.models import Label
 from images.models import Source, Value1, Value2, Value3, Value4, Value5, Metadata
+from django.forms.extras.widgets import SelectDateWidget
 
 MODE_CHOICES = (
     ('image', 'View whole images'),
     ('patch', 'View patches'),
 )
+
+DATE_CHOICES = ()
+class YearModelChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, Metadata):
+        return Metadata.photo_date.year
+        
 
 class VisualizationSearchForm(forms.Form):
     def __init__(self,source_id,*args,**kwargs):
@@ -19,16 +27,14 @@ class VisualizationSearchForm(forms.Form):
         self.fields['value3'].queryset = Value3.objects.filter(source=source)
         self.fields['value4'].queryset = Value4.objects.filter(source=source)
         self.fields['value5'].queryset = Value5.objects.filter(source=source)
-        self.fields['year'].queryset = Metadata.objects.filter(Image__source=source).distinct()
+        self.fields['year'].queryset = Metadata.objects.filter(image__source=source).distinct()
         self.fields['labels'].queryset = Label.objects.filter(labelset__id=source.labelset_id).distinct()
 
-    #value1 = forms.ChoiceField(choices=(), widget=forms.Select())
-    #source = get_object_or_404(Source,id=source_id)
     value1 = forms.ModelChoiceField(queryset=(), empty_label="All", required=False)
     value2 = forms.ModelChoiceField(queryset=(), empty_label="All", required=False)
     value3 = forms.ModelChoiceField(queryset=(), empty_label="All", required=False)
     value4 = forms.ModelChoiceField(queryset=(), empty_label="All", required=False)
     value5 = forms.ModelChoiceField(queryset=(), empty_label="All", required=False)
-    year = forms.ModelChoiceField(queryset=(), empty_label="All", required=False)
+    year = YearModelChoiceField(queryset=(), empty_label="All", required=False)
     mode = forms.ChoiceField(choices=MODE_CHOICES)
     labels = forms.ModelChoiceField(queryset=(), empty_label="All", required=False)
