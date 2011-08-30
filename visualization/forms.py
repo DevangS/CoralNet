893b9d726c619c3a535a2ff679e08bfe4a723b68
Devang Sampat <devangs27@gmail.com>
2011-08-29 21:22:53 -0700

    Fixed a bug in visualization tool where Value1 was being looked at instead of Value2-5

--- visualization/forms.py
+++ visualization/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms.fields import DateField
 from django.shortcuts import get_object_or_404
-from annotations.models import Label
+from annotations.models import Label, LabelSet
 from images.models import Source, Value1, Value2, Value3, Value4, Value5, Metadata
 from django.forms.extras.widgets import SelectDateWidget
 
@@ -32,13 +32,14 @@ class VisualizationSearchForm(forms.Form):
             self.fields[gSource.key4].queryset = Value4.objects.filter(source=gSource)
         if gSource.key5:
             self.fields[gSource.key5].queryset = Value5.objects.filter(source=gSource)
-        self.fields['year'].queryset = Metadata.objects.filter(image__source=gSource).distinct()
-        self.fields['label'].queryset = Label.objects.filter(labelset__id=gSource.labelset_id).distinct()
+        self.fields['year'].queryset = Metadata.objects.filter(image__source=gSource).distinct() #need to find distinct year
+        labelset = LabelSet.objects.filter(source=gSource)[0]
+        self.fields['labels'].queryset = labelset.labels.all()
 
     global gSource
-    
+
     #These checks are redundant with the field ones above, but after spending 2 hours on this issue,
-    # it's the best I came up with and there's a deadline to meet so TODO: optimize by trying to use get method on vars()
+    # it's the best I came up with and there's a deadline to meet so TODO: optimize
     if gSource.key1:
         vars()[gSource.key1] = forms.ModelChoiceField(queryset=(), empty_label="All", required=False)
     if gSource.key2:
@@ -49,6 +50,6 @@ class VisualizationSearchForm(forms.Form):
         vars()[gSource.key4] = forms.ModelChoiceField(queryset=(), empty_label="All", required=False)
     if gSource.key5:
         vars()[gSource.key5] = forms.ModelChoiceField(queryset=(), empty_label="All", required=False)
-        
+
     year = YearModelChoiceField(queryset=(), empty_label="All", required=False)
-    label = forms.ModelChoiceField(queryset=(), empty_label="View Whole Images", required=False)
+    labels = forms.ModelChoiceField(queryset=(), empty_label="All", required=False)
