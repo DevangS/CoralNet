6ec0901807add612076b44992d0ac038617d436e
Devang Sampat <devangs27@gmail.com>
2011-08-30 01:38:56 -0700

    Changed the implementation of year yet to try to get it to work correctly

--- visualization/forms.py
+++ visualization/forms.py
@@ -24,13 +24,13 @@ class VisualizationSearchForm(forms.Form):
         super(VisualizationSearchForm,self).__init__(*args,**kwargs)
         source = Source.objects.filter(id=source_id)[0]
         metadatas = Metadata.objects.filter(image__source=source).distinct().dates('photo_date', 'year')
-        years = ["All"]
+        years = []
         for metadata in metadatas:
             if metadata:
                 if not metadata.year in years:
                     years.append(metadata.year)
 
-        self.fields['year'] = ChoiceField(choices=[(year,year) for year in years],
+        self.fields['year'] = ChoiceField(choices=[('',"All")] + [(year,year) for year in years],
                                           required=False)
 
         labelset = LabelSet.objects.filter(source=source)[0]
