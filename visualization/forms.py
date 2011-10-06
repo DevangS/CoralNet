0cb63749054b4b71bf21b475a695d408a3ee32aa
Stephen Chan <stephenjchan@gmail.com>
2011-10-06 15:33:19 -0700

    - Image delete in the visualization page almost works - still need to:   1. Implement an "Are you sure you want to delete these n images?" message, perhaps in a JavaScript alert.   2. Actually make it delete the selected images (right now it just retrieves the images to delete). - If incorrect search parameters are entered (this only happens with direct URL typing), there will be an appropriate error message, "There were errors in the search parameters.". - Visualization view code refactored in general (hopefully for the better).

--- visualization/forms.py
+++ visualization/forms.py
@@ -1,14 +1,8 @@
 from django import forms
-from django.forms.fields import DateField, ChoiceField, CharField
-from django.forms.widgets import TextInput
-from django.shortcuts import get_object_or_404
-from annotations.models import Label, LabelSet
+from django.forms.fields import ChoiceField, CharField
+from django.forms.widgets import HiddenInput
+from annotations.models import LabelSet
 from images.models import Source, Value1, Value2, Value3, Value4, Value5, Metadata
-from django.forms.extras.widgets import SelectDateWidget
-
-DATE_CHOICES = ()
-
-#gSource = get_object_or_404(Source, id=1)
 
 class YearModelChoiceField(forms.ModelChoiceField):
     def label_from_instance(self, Metadata):
@@ -52,41 +46,18 @@ class VisualizationSearchForm(forms.Form):
                 
                 self.fields[valueField] = ChoiceField(choices, label=key, required=False)
 
-           # else:
-            #    del self.fields[valueField]
 
-"""        global gSource
-        gSource = get_object_or_404(Source,id=source_id)
+class ImageBatchActionForm(forms.Form):
+    action = ChoiceField(
+        label="Action",
+        choices=(
+            ('', '---------'),
+            ('delete', 'Delete'),
+        ),
+        error_messages = {
+            'required': 'No action selected.',
+        }
+    )
 
-        #Gets options to show for each value that the user selects
-        if gSource.key1:
-            self.fields[gSource.key1].queryset = Value1.objects.filter(source=gSource)
-        if gSource.key2:
-            self.fields[gSource.key2].queryset = Value2.objects.filter(source=gSource)
-        if gSource.key3:
-            self.fields[gSource.key3].queryset = Value3.objects.filter(source=gSource)
-        if gSource.key4:
-            self.fields[gSource.key4].queryset = Value4.objects.filter(source=gSource)
-        if gSource.key5:
-            self.fields[gSource.key5].queryset = Value5.objects.filter(source=gSource)
-        self.fields['year'].queryset = Metadata.objects.filter(image__source=gSource).distinct() #need to find distinct year
-        labelset = LabelSet.objects.filter(source=gSource)[0]
-        self.fields['labels'].queryset = labelset.labels.all()
-
-    global gSource
-
-    #These checks are redundant with the field ones above, but after spending 2 hours on this issue,
-    # it's the best I came up with and there's a deadline to meet so TODO: optimize
-    if gSource.key1:
-        vars()[gSource.key1] = forms.ModelChoiceField(queryset=(), empty_label="All", required=False)
-    if gSource.key2:
-        vars()[gSource.key2] = forms.ModelChoiceField(queryset=(), empty_label="All", required=False)
-    if gSource.key3:
-        vars()[gSource.key3] = forms.ModelChoiceField(queryset=(), empty_label="All", required=False)
-    if gSource.key4:
-        vars()[gSource.key4] = forms.ModelChoiceField(queryset=(), empty_label="All", required=False)
-    if gSource.key5:
-        vars()[gSource.key5] = forms.ModelChoiceField(queryset=(), empty_label="All", required=False)
-
-    year = YearModelChoiceField(queryset=(), empty_label="All", required=False)
-    labels = forms.ModelChoiceField(queryset=(), empty_label="All", required=False) """
+    # The search keys as a JSON-ized dictionary
+    searchKeys = CharField(widget=HiddenInput())
--- visualization/views.py
+++ visualization/views.py
@@ -1,11 +1,42 @@
+from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, InvalidPage
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template.context import RequestContext
+from django.utils import simplejson
 from annotations.models import Annotation, Label
 from CoralNet.decorators import visibility_required
 from images.models import Source, Image
-from visualization.forms import VisualizationSearchForm
+from visualization.forms import VisualizationSearchForm, ImageBatchActionForm
 from visualization.utils import generate_patch_if_doesnt_exist
+
+
+def image_search_args_to_queryset_args(searchDict):
+    """
+    Take the image search arguments directly from the visualization search form's
+    form.cleaned_data, and return the search arguments in a format that can go into
+    Image.objects.filter().
+
+    Only the value1, ... valuen and the year in cleaned_data are processed.  label and
+    page are ignored and not included in the returned dict.
+    """
+
+    searchArgs = dict([(k, searchDict[k]) for k in searchDict if searchDict[k] != ''])
+    querysetArgs = dict()
+
+    for k in searchArgs:
+        if k.startswith('value'):
+            querysetArgs['metadata__'+k+'__id'] = searchArgs[k]
+        elif k == 'year':
+            querysetArgs['metadata__photo_date__'+k] = int(searchArgs[k])
+
+    return querysetArgs
+
+
+def image_search_args_to_url_arg_str(searchDict):
+    return '&'.join(['%s=%s' % (paramName, searchDict[paramName])
+                     for paramName in searchDict
+                     if searchDict[paramName]    # Don't include the arg if it's None or ''
+                        and paramName != 'page'])
 
 
 @visibility_required('source_id')
@@ -15,125 +46,127 @@ def visualize_source(request, source_id):
     """
     IMAGES_PER_PAGE = 20
 
-    kwargs = {
-        #this will contain the parameters to filter the images by
-        #that way the filter args can be dynamically generated if
-        #all possible values are not filled
-    }
-
-    pargs = {
-        #same as above but needs different indexes because it's used
-        #for patch mode. TODO: reimplement this in a non-hackish way
-    }
-
-    errors = [] #keeps tracks of errors that occur
-    all_images = [] #holds all the images to display
+    searchFormErrors = False
     source = get_object_or_404(Source, id=source_id)
-    kwargs['source'] = source
-    searchParamsStr = ''  # GET params in url format - for constructing prev page/next page links
-    will_generate_patches = False
 
+    urlArgsStr = ''  # GET args in url format - for constructing prev page/next page links
+    label = False
+    imageArgs = dict()
+
+    # Get image search filters, if any
     if request.GET:
 
         #form to select descriptors to sort images
         form = VisualizationSearchForm(source_id, request.GET)
+
         if form.is_valid():
-            value1Index = request.GET.get('value1', 0)
-            value2Index = request.GET.get('value2', 0)
-            value3Index = request.GET.get('value3', 0)
-            value4Index = request.GET.get('value4', 0)
-            value5Index = request.GET.get('value5', 0)
-            year = request.GET.get('year', '')
-            label = request.GET.get('labels', '')
 
-            if value1Index:
-                kwargs['metadata__value1__id'] = value1Index
-                pargs['image__metadata__value1__id'] = value1Index
-            if value2Index:
-                kwargs['metadata__value2__id'] = value2Index
-                pargs['image__metadata__value2__id'] = value2Index
-            if value3Index:
-                kwargs['metadata__value3__id'] = value3Index
-                pargs['image__metadata__value3__id'] = value3Index
-            if value4Index:
-                kwargs['metadata__value4__id'] = value4Index
-                pargs['image__metadata__value4__id'] = value4Index
-            if value5Index:
-                kwargs['metadata__value5__id'] = value5Index
-                pargs['image__metadata__value5__id'] = value5Index
-            if year != "All" and year:
-                kwargs['metadata__photo_date__year'] = int(year)
-                pargs['image__metadata__photo_date__year'] = int(year)
+            urlArgsStr = image_search_args_to_url_arg_str(form.cleaned_data)
+            imageArgs = image_search_args_to_queryset_args(form.cleaned_data)
 
-            if not label:
-                all_images = Image.objects.filter(**kwargs).order_by('-upload_date')
-            else:
-                #get all annotations for the source that contain the label
-                label = get_object_or_404(Label, id=label)
-                annotations = Annotation.objects.filter(source=source, label=label, **pargs)
-                #TODO: add searching annotations based on key/value pairs
+            label = form.cleaned_data['labels']
 
-                # Placeholder the image patches with the annotation objects for now.
-                # We'll actually get the patches when we know which page we're showing.
-                all_images = annotations
-                will_generate_patches = True
-
-            searchParams = dict(request.GET)
-            if searchParams.has_key('page'):
-                del searchParams['page']
-            searchParamsStr = '&'.join(['%s=%s' % (paramName, searchParams[paramName][0])
-                                     for paramName in searchParams])
+        else:
+            searchFormErrors = True
 
     else:
         form = VisualizationSearchForm(source_id)
-        all_images = Image.objects.filter(source=source).order_by('-upload_date')
 
-    if not all_images and not errors:
-        if request.GET:
-            errors.append("Sorry, no images matched your query")
+
+    # Perform selected actions, if any, on the images previously shown
+    if request.POST:
+        actionForm = ImageBatchActionForm(request.POST)
+
+        if actionForm.is_valid():
+            if actionForm.cleaned_data['action'] == 'delete':
+                actionFormImageArgs = simplejson.loads(actionForm.cleaned_data['searchKeys'])
+                imagesToDelete = Image.objects.filter(source=source, **actionFormImageArgs)
+
+                for img in imagesToDelete:
+                    pass
+                    #img.delete()
+
+                # Note that img.delete() just removes the image from the
+                # database.  But the image objects are still there in the
+                # imagesToDelete list (they just don't have primary keys anymore).
+                messages.success(request, 'The %d selected images have been deleted.' % len(imagesToDelete))
+    else:
+        actionForm = ImageBatchActionForm(initial={'searchKeys': simplejson.dumps(imageArgs)})
+
+
+    # Get the search results (all of them, not just on this page)
+    # (This must happen after processing the action form, so that
+    # images can be deleted/modified before we get search results.)
+
+    errors = []
+    if searchFormErrors:
+        errors.append("There were errors in the search parameters.")
+        images = []
+        showPatches = False
+    else:
+        if not label:
+            showPatches = False
+            allSearchResults = Image.objects.filter(source=source, **imageArgs).order_by('-upload_date')
         else:
-            errors.append("Sorry, there are no images for this source yet. Please upload some.")
+            showPatches = True
+            patchArgs = dict([('image__'+k, imageArgs[k]) for k in imageArgs])
 
-    paginator = Paginator(all_images, IMAGES_PER_PAGE)
+            #get all annotations for the source that contain the label
+            annotations = Annotation.objects.filter(source=source, label=label, **patchArgs)
 
-    # Make sure page request is an int. If not, deliver first page.
-    try:
-        page = int(request.GET.get('page', '1'))
-    except ValueError:
-        page = 1
+            # Placeholder the image patches with the annotation objects for now.
+            # We'll actually get the patches when we know which page we're showing.
+            allSearchResults = annotations
 
-    # If page request (9999) is out of range, deliver last page of results.
-    try:
-        images = paginator.page(page)
-    except (EmptyPage, InvalidPage):
-        images = paginator.page(paginator.num_pages)
+        if not allSearchResults:
+            if request.GET:
+                # No image results in this search
+                errors.append("Sorry, no images matched your query")
+            else:
+                # No image results, and just got to the visualization page
+                errors.append("Sorry, there are no images for this source yet. Please upload some.")
 
-    if will_generate_patches:
-        
-        # Get an image-patch for each result on the page.
-        # Earlier we placeholdered the image patches with the annotation objects,
-        # so we're iterating over those annotations now.
-        for index, annotation in enumerate(images.object_list):
+        paginator = Paginator(allSearchResults, IMAGES_PER_PAGE)
 
-            patchPath = "data/annotations/" + str(annotation.id) + ".jpg"
+        # Make sure page request is an int. If not, deliver first page.
+        try:
+            page = int(request.GET.get('page', '1'))
+        except ValueError:
+            page = 1
 
-            images.object_list[index] = dict(
-                fullImage=annotation.image,
-                patchPath=patchPath,
-                row=annotation.point.row,
-                col=annotation.point.column,
-                pointNum=annotation.point.point_number,
-            )
+        # If page request (9999) is out of range, deliver last page of results.
+        try:
+            images = paginator.page(page)
+        except (EmptyPage, InvalidPage):
+            images = paginator.page(paginator.num_pages)
 
-            generate_patch_if_doesnt_exist(patchPath, annotation)
+        if showPatches:
+
+            # Get an image-patch for each result on the page.
+            # Earlier we placeholdered the image patches with the annotation objects,
+            # so we're iterating over those annotations now.
+            for index, annotation in enumerate(images.object_list):
+
+                patchPath = "data/annotations/" + str(annotation.id) + ".jpg"
+
+                images.object_list[index] = dict(
+                    fullImage=annotation.image,
+                    patchPath=patchPath,
+                    row=annotation.point.row,
+                    col=annotation.point.column,
+                    pointNum=annotation.point.point_number,
+                )
+
+                generate_patch_if_doesnt_exist(patchPath, annotation)
 
     return render_to_response('visualization/visualize_source.html', {
         'errors': errors,
         'form': form,
         'source': source,
         'images': images,
-        'searchParamsStr': searchParamsStr,
+        'showPatches': showPatches,
+        'searchParamsStr': urlArgsStr,
+        'actionForm': actionForm,
         },
         context_instance=RequestContext(request)
-        
     )
