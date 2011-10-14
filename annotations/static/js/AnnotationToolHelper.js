46e009c85552aa57f7661be0a6fe08287373f112
Stephen Chan <stephenjchan@gmail.com>
2011-10-13 22:29:19 -0700

    Annotation tool: - Ctrl-click to select points on the image now works, and points are different colors when selected vs. not selected. - Some refactoring of the JavaScript (still need better function organization though).

--- annotations/static/js/AnnotationToolHelper.js
+++ annotations/static/js/AnnotationToolHelper.js
@@ -6,6 +6,7 @@ var AnnotationToolHelper = {
     annotationFields: [],
     coralImage: null,
     pointsCanvas: null,
+    listenerElmt: null,
     saveButton: null,
 
     // Annotation related
@@ -13,7 +14,7 @@ var AnnotationToolHelper = {
 
     // Canvas related
 	context: null,
-    points: null, imagePoints: null,
+    canvasPoints: [], imagePoints: null,
 	POINT_RADIUS: 16,
     NUMBER_FONT: "bold 24px sans-serif",
 
@@ -24,9 +25,11 @@ var AnnotationToolHelper = {
 
 	// Color for unselected points (point selection to be implemented)
     UNANNOTATED_COLOR: "#FFFF00",
-	UNANNOTATED_COLOR_OUTLINE: "#000000",
-	ANNOTATED_COLOR: "#FF0000",
-	SELECTED_COLOR: "#888888",
+	UNANNOTATED_OUTLINE_COLOR: "#000000",
+	SELECTED_COLOR: "#00FF00",
+    SELECTED_OUTLINE_COLOR: "#000000",
+	ANNOTATED_COLOR: "#8888FF",
+    ANNOTATED_OUTLINE_COLOR: "#000000",
 
 	// We should automatically match the coral image's width/height ratio and support up to a maximum width/height.
 	IMAGE_DISPLAY_HEIGHT: null,
@@ -36,7 +39,7 @@ var AnnotationToolHelper = {
 
     init: function(initialHeight, initialWidth,
                    fullHeight, fullWidth,
-                   points, labelCodes) {
+                   imagePoints, labelCodes) {
         var t = this;  // Alias for less typing
 
         t.IMAGE_DISPLAY_HEIGHT = initialHeight;
@@ -48,6 +51,7 @@ var AnnotationToolHelper = {
         t.annotationList = $("#annotationList")[0];
         t.coralImage = $("#coralImage")[0];
         t.pointsCanvas = $("#pointsCanvas")[0];
+        t.listenerElmt = $("#listenerElmt")[0];
         t.saveButton = $("#saveButton")[0];
 
         t.labelCodes = labelCodes;
@@ -77,6 +81,17 @@ var AnnotationToolHelper = {
             "z-index": 1
         });
 
+        // Invisible element that goes over the coral image
+        // and listens for mouseclicks.
+        // z-index should be above everything else.
+        $(t.listenerElmt).css({
+            "width": t.IMAGE_DISPLAY_WIDTH + "px",
+            "height": t.IMAGE_DISPLAY_HEIGHT + "px",
+            "left": t.CANVAS_GUTTER + "px",
+            "top": t.CANVAS_GUTTER + "px",
+            "z-index": 100
+        });
+
         // Note that the canvas's width and height elements are different from the
         // canvas style's width and height. We're interested in the canvas width and height,
         // so the canvas contents don't stretch.
@@ -90,13 +105,18 @@ var AnnotationToolHelper = {
         t.context.translate(t.CANVAS_GUTTER, t.CANVAS_GUTTER);
 
 		// Mouse button is pressed and un-pressed
-		t.pointsCanvas.addEventListener("mouseup", t.onMouseup, false);
+		$(t.listenerElmt).mouseup( function(e) {
+            // TODO: This shouldn't happen if the points display is toggled off
+            if(e.ctrlKey) {
+                var nearestPoint = AnnotationToolHelper.getNearestPoint(e);
+                AnnotationToolHelper.toggle([nearestPoint]);
+            }
+        });
 
         // Initialize points
-        t.imagePoints = points;
-        t.points = new Array(points.length);
+        t.imagePoints = imagePoints;
         t.getCanvasPoints();
-        t.drawPoints();
+        t.drawAllPoints();
 
         // Initialize save button
         $(t.saveButton).removeAttr('disabled');  // Firefox might cache this attribute between page loads
@@ -116,8 +136,7 @@ var AnnotationToolHelper = {
 
         // Create array that maps point numbers to annotation form fields:
         // annotationFields[1] = field with name "label_1", etc.
-        // (It's used like an associative array, but it's really a regular array
-        // because it has numeric indices.)
+        // (It's used like an associative array.)
         annotationFieldsJQ.each( function() {
             var pointNum = AnnotationToolHelper.getPointNumOfAnnoField(this);
             AnnotationToolHelper.annotationFields[pointNum] = this;
@@ -161,12 +180,13 @@ var AnnotationToolHelper = {
 
     /* Get the mouse position in the canvas element:
 	(mouse's position in the HTML document) minus
-	(canvas element's position in the HTML document).
-	The method for getting the mouse position in the HTML document
-	varies depending on the browser: can be based on pageX/Y or clientX/Y. */
-	getMousePosition: function(e) {
+	(canvas element's position in the HTML document). */
+	getCanvasPosition: function(e) {
 	    var x;
 		var y;
+
+		/* The method for getting the mouse position in the HTML document
+	    varies depending on the browser: can be based on pageX/Y or clientX/Y. */
 		if (e.pageX != undefined && e.pageY != undefined) {
 			x = e.pageX;
 			y = e.pageY;
@@ -178,18 +198,53 @@ var AnnotationToolHelper = {
 					document.documentElement.scrollTop;
 		}
 
-		x -= this.pointsCanvas.offsetLeft;
-		y -= this.pointsCanvas.offsetTop;
+        // Get the x,y relative to the upper-left corner of the coral image
+        var elmt = this.pointsCanvas;
+        while (elmt != null) {
+            x -= elmt.offsetLeft;
+            y -= elmt.offsetTop;
+            elmt = elmt.offsetParent;
+        }
 
 	    return [x,y];  // Return an array
 	},
 
-    /*
-    TODO: Implement point-selection and other mouseclick functionality here.
-     */
-	onMouseup: function(e) {
+    getImagePosition: function(e) {
+        var t = this;
+        var canvasPosition = t.getCanvasPosition(e);
+        var canvasX = canvasPosition[0];
+        var canvasY = canvasPosition[1];
 
-	},
+        var x = (canvasX - t.CANVAS_GUTTER) * (t.IMAGE_FULL_WIDTH / t.IMAGE_DISPLAY_WIDTH);
+        var y = (canvasY - t.CANVAS_GUTTER) * (t.IMAGE_FULL_HEIGHT / t.IMAGE_DISPLAY_HEIGHT);
+        
+        return [x,y];
+    },
+
+    getNearestPoint: function(e) {
+        var t = this;
+
+        // Mouse's position in the canvas element
+        var imagePosition = this.getImagePosition(e);
+        var x = imagePosition[0];
+        var y = imagePosition[1];
+
+        var minDistance = Infinity;
+        var closestPoint = null;
+
+        for (var i = 0; i < t.imagePoints.length; i++) {
+            var xDiff = x - t.imagePoints[i].column;
+            var yDiff = y - t.imagePoints[i].row;
+            var distance = Math.sqrt((xDiff*xDiff) + (yDiff*yDiff));
+
+            if (distance < minDistance) {
+                minDistance = distance;
+                closestPoint = t.imagePoints[i].point_number;
+            }
+        }
+
+        return closestPoint;  //TODO: Change
+    },
 
     /*
     To get the points in canvas coordinates,
@@ -201,8 +256,9 @@ var AnnotationToolHelper = {
         // Apparently JavaScript uses decimal division by default, which we want in this case.
         var scaleFactor = t.IMAGE_DISPLAY_WIDTH / t.IMAGE_FULL_WIDTH;
 
-        for (var i = 0; i < t.points.length; i++) {
-            t.points[i] = {
+        for (var i = 0; i < t.imagePoints.length; i++) {
+
+            t.canvasPoints[t.imagePoints[i].point_number] = {
                 num: t.imagePoints[i].point_number,
                 row: t.imagePoints[i].row * scaleFactor,
                 col: t.imagePoints[i].column * scaleFactor
@@ -210,20 +266,43 @@ var AnnotationToolHelper = {
         }
     },
 
-    drawPoints: function() {
+    drawAllPoints: function() {
         var t = this;
         
-        for (var i = 0; i < t.points.length; i++) {
-            t.drawAnnotationPoint(t.points[i].col, t.points[i].row);
-            t.drawNumber(t.points[i].num.toString(), t.points[i].col, t.points[i].row);
+        for (var pointNum in t.canvasPoints) {
+
+            // hasOwnProperty() is a safety check that's useful when using for...in
+            if (t.canvasPoints.hasOwnProperty(pointNum)) {
+
+                t.drawPointUnannotated(pointNum);
+            }
         }
     },
 
+    drawPoint: function(pointNum, color, outlineColor) {
+        var canvasPoint = this.canvasPoints[pointNum];
+
+        this.drawPointSymbol(canvasPoint.col, canvasPoint.row,
+                             color);
+        this.drawPointNumber(canvasPoint.num.toString(), canvasPoint.col, canvasPoint.row,
+                             color, outlineColor);
+    },
+
+    drawPointUnannotated: function(pointNum) {
+        this.drawPoint(pointNum, this.UNANNOTATED_COLOR, this.UNANNOTATED_OUTLINE_COLOR);
+    },
+    drawPointAnnotated: function(pointNum) {
+        this.drawPoint(pointNum, this.ANNOTATED_COLOR, this.ANNOTATED_OUTLINE_COLOR);
+    },
+    drawPointSelected: function(pointNum) {
+        this.drawPoint(pointNum, this.SELECTED_COLOR, this.SELECTED_OUTLINE_COLOR);
+    },
+
     /*
-    Draw an annotation point (the crosshair, circle, etc.)
+    Draw an annotation point (the crosshair, circle, or whatever)
     which is centered at x,y.
      */
-    drawAnnotationPoint: function(x,y) {
+    drawPointSymbol: function(x, y, color) {
         var t = this;
 
 		// Adjust x and y by 0.5 so that straight lines are centered
@@ -234,7 +313,7 @@ var AnnotationToolHelper = {
 		x = x+0.5;
 		y = y+0.5;
 
-        t.context.strokeStyle = t.UNANNOTATED_COLOR;
+        t.context.strokeStyle = color;
         t.context.lineWidth = 3;
 
 		t.context.beginPath();
@@ -253,13 +332,13 @@ var AnnotationToolHelper = {
     Draw the number of an annotation point
     which is centered at x,y.
      */
-	drawNumber: function(num, x, y) {
+	drawPointNumber: function(num, x, y, color, outlineColor) {
         var t = this;
 
 		t.context.textBaseline = "bottom";
 		t.context.textAlign = "left";
-		t.context.fillStyle = t.UNANNOTATED_COLOR;
-        t.context.strokeStyle = t.UNANNOTATED_COLOR_OUTLINE;
+		t.context.fillStyle = color;
+        t.context.strokeStyle = outlineColor;
         t.context.lineWidth = 1;
 	    t.context.font = t.NUMBER_FONT;
 
@@ -320,20 +399,34 @@ var AnnotationToolHelper = {
     },
 
     select: function(points) {
+
         for (var i = 0; i < points.length; i++) {
             $(this.annotationFields[points[i]]).addClass('selected');
+            this.drawPointSelected(points[i]);
         }
     },
 
     unselect: function(points) {
+
         for (var i = 0; i < points.length; i++) {
             $(this.annotationFields[points[i]]).removeClass('selected');
+            this.drawPointUnannotated(points[i]);
+        }
+    },
+
+    toggle: function(points) {
+        for (var i = 0; i < points.length; i++) {
+            if ($(this.annotationFields[points[i]]).hasClass('selected'))
+                this.unselect([points[i]]);
+            else
+                this.select([points[i]]);
         }
     },
 
     unselectAll: function() {
         this.getSelectedFieldsJQ().each( function() {
-            $(this).removeClass('selected');
+            var pointNum = AnnotationToolHelper.getPointNumOfAnnoField(this);
+            AnnotationToolHelper.unselect([pointNum]);
         });
     },
 
