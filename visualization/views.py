2a589a5fd4595f69d0ab87e312b803dfc4826f21
Devang Sampat <devangs27@gmail.com>
2011-12-17 16:21:45 -0800

    Made Y axis show percent coverage in multiples of 5

--- visualization/views.py
+++ visualization/views.py
@@ -285,7 +285,7 @@ def generate_statistics(request, source_id):
                 #draw x axis values from lowest to highest year stepping by 1 year
                 graph.axes.range(0,min(years),max(years),1)
                 #draw y axis values from 0 to 100 stepping by 10
-                graph.axes.range(1,0,100,10)
+                graph.axes.range(1,0,100,5)
                 #Define pixel size to draw graph
                 graph.size(500,500)
 
