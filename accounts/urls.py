5cf762c27a47e82d2ca180bbb1bd0dd8c3519137
Devang Sampat <devangs27@gmail.com>
2011-12-21 15:34:40 -0800

    Made minor change to requesting invite functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *  # 404, 500, patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^invite/$', 'accounts.views.request_invite', name="request_invite"),
+    url(r'^invite/', 'accounts.views.request_invite', name="request_invite"),
 )
