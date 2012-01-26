0086b3d702a1a5e663a0ed337b61b01ebb7a577d
Devang Sampat <devangs27@gmail.com>
2012-01-26 11:41:21 -0800

    Testing out an optimization to exporting statistics

--- visualization/views.py
+++ visualization/views.py
@@ -361,6 +361,7 @@ def export_statistics(request, source_id):
         total_annotations_count = image_annotations.count()
 
         for label_index, label in enumerate(labels):
+            #Testing out this optimization to see if it's any faster
             label_annotations_count = all_annotations.filter(image=image, label=label).count()
             try:
                 label_percent_coverage = (float(label_annotations_count)/total_annotations_count)*100
