208aba4ce5060c45d5cc227442ed9a37366d78a3
obeijbom <obeijbom@ucsd.edu>
2012-03-15 19:15:18 -0700

    updated task_helpers

--- images/task_helpers_example.py
+++ images/task_helpers_example.py
@@ -13,20 +13,16 @@
 #
 # If you add a function to task_helpers.py, add it to this example file
 # too, so other team members know what needs to go in their task_helpers.py.
-
-
-def coralnet_preprocessImage(imageFile, preprocessedImageFile, preprocessParameterFile, errorLogfile):
+def coralnet_preprocessImage(imageFile, preprocessedImageFile, preprocessParameterFile, ssFile , logFile, errorLogfile):
     # The Preprocessing step prepares the image for feature making.
-    pass
+	pass
 
-
-def coralnet_makeFeatures(preprocessedImageFile, featureFile, rowColFile, featureExtractionParameterFile, errorLogfile):
+def coralnet_makeFeatures(preprocessedImageFile, featureFile, rowColFile, ssFile, featureExtractionParameterFile, logFile, errorLogfile):
     # The Feature file encodes the visual features around each point.
-    pass
+	pass
 
-
-def coralnet_classify(featureFile, modelFile, labelFile, errorLogfile):
-    # The label file is the result of classification: what each point is labelled as.
+def coralnet_classify(featureFile, modelFile, labelFile, logFile, errorLogfile):
+	# The label file is the result of classification: what each point is labelled as.
     # From the feature file, we get information about the points.
     # From the model file, we figure out how to label a point given its features.
     #
@@ -34,3 +30,8 @@ def coralnet_classify(featureFile, modelFile, labelFile, errorLogfile):
     # made by any tasks.  The model file also needs to be set as the path_to_model of
     # the Robot that's being used.
     pass
+
+def coralnet_trainRobot(modelPath, oldModelPath, pointInfoPath, fileNamesPath, workDir, logFile, errorLogfile):
+	# modelPath is the path the the new robotMode, the oldModelpath is the path to the previous. pointInfoPath contains info about each point, label and fromfile. filenamesPath is a list of the feature fiels. workDir is a temp directory for matlab to work in.
+	pass
+
