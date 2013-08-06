dd4f882ca284c8637c6d036da4fe31d5ec5c54b8
Stephen Chan <stephenjchan@gmail.com>
2013-08-06 12:48:52 -0700

    Browse: refactored VisSearchFormHelper.js to be more like the Upload page's Javascript structuring.

--- visualization/static/js/VisSearchFormHelper.js
+++ visualization/static/js/VisSearchFormHelper.js
@@ -1,38 +1,45 @@
-var VSH = {
+var BrowseHelper = (function() {
 
-    $pageViewField: undefined,
-    $imagePatchViewFieldsWrapper: undefined,
-    $imageOrMetadataViewFieldsWrapper: undefined,
+    var $pageViewChoices;
+    var $pageViewPatchesChoice;
+    var $imagePatchViewFieldsWrapper;
+    var $imageOrMetadataViewFieldsWrapper;
 
-    sourceUsesMachineAnnotator: undefined,
+    var sourceUsesMachineAnnotator;
 
-    init: function(sourceUsesMachineAnnotator) {
-        VSH.$pageViewChoices = $('#id_page_view_wrapper input[type="radio"]');
-        VSH.$pageViewPatchesChoice = $('#id_page_view_wrapper input[value="patches"]');
-
-        VSH.$imagePatchViewFieldsWrapper = $('#id_image_patch_view_fields_wrapper');
-        VSH.$imageOrMetadataViewFieldsWrapper = $('#id_image_or_metadata_view_fields_wrapper');
-        VSH.sourceUsesMachineAnnotator = sourceUsesMachineAnnotator;
-
-        // Based on initial value of the page view field, show one group
-        // of fields or the other.
-        VSH.displayStatusOrAnnotatorField();
-
-        // When any of the radio buttons are clicked, show/hide the appropriate fields.
-        VSH.$pageViewChoices.click(VSH.displayStatusOrAnnotatorField);
-    },
-
-    displayStatusOrAnnotatorField: function() {
-        if (VSH.$pageViewPatchesChoice.prop('checked') === true) {
+    function displayStatusOrAnnotatorField() {
+        if ($pageViewPatchesChoice.prop('checked') === true) {
             // Image patches
-            // TODO: Account for VSH.sourceUsesMachineAnnotator to show the annotated_by field or not?
-            VSH.$imagePatchViewFieldsWrapper.show();
-            VSH.$imageOrMetadataViewFieldsWrapper.hide();
+            $imagePatchViewFieldsWrapper.show();
+            $imageOrMetadataViewFieldsWrapper.hide();
         }
         else {
             // Images or metadata
-            VSH.$imagePatchViewFieldsWrapper.hide();
-            VSH.$imageOrMetadataViewFieldsWrapper.show();
+            $imagePatchViewFieldsWrapper.hide();
+            $imageOrMetadataViewFieldsWrapper.show();
         }
     }
-};
+
+    return {
+
+        init: function(sourceUsesMachineAnnotator) {
+            $pageViewChoices = $('#id_page_view_wrapper input[type="radio"]');
+            $pageViewPatchesChoice = $('#id_page_view_wrapper input[value="patches"]');
+
+            $imagePatchViewFieldsWrapper = $('#id_image_patch_view_fields_wrapper');
+            $imageOrMetadataViewFieldsWrapper = $('#id_image_or_metadata_view_fields_wrapper');
+
+            // TODO: If this is false, don't show the Annotator field?
+            sourceUsesMachineAnnotator = sourceUsesMachineAnnotator;
+
+            // Based on initial value of the page view field, show one group
+            // of fields or the other.
+            displayStatusOrAnnotatorField();
+
+            // When any of the radio buttons are clicked, show/hide the appropriate fields.
+            $pageViewChoices.click(function() {
+                displayStatusOrAnnotatorField();
+            });
+        }
+    }
+})();
