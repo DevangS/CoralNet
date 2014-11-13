0a3f345e02969e94df64faef1f3f9b4596279442
obeijbom <obeijbom@ucsd.edu>
2014-11-13 13:51:22 -0800

    added a resetImageStatus file

--- images/scripts.py
+++ images/scripts.py
@@ -1,8 +1,10 @@
 from images.models import Point, Image, Source, Robot
 import os
+from django.conf import settings
 
-
-
+join_processing_root = lambda *p: os.path.join(settings.PROCESSING_ROOT, *p)
+PREPROCESS_DIR = join_processing_root("images/preprocess/")
+FEATURES_DIR = join_processing_root("images/features/")
 
 """
 This is a script to remove all inactive robot models. 
@@ -28,4 +30,26 @@ def removeOldRobots():
                 except:
                     print this_robot.path_to_model + "is already removed"
     
+"""
+This script was created after a week of MATLAB not starting. This created a bunch of images with the wrong status. For example, the database updated status.featuresExtracted = true, and then called MATLAB. MATLAB then didn't start (and didn't tell the system), so the status was never changed back. The remedy is to go through all images and check that the MATLAB output files are on the disk.
+"""
     
+def resetImageStatus():
+    source_querry = Source.objects.filter()
+    for this_source in source_querry:
+        imgquerry = Image.objects.filter(source = this_source)
+        img_counter = 0
+        for image in imgquerry:
+            if image.process_date is None:
+                img_counter += 1
+                continue
+            preprocessedImageFile = os.path.join(PREPROCESS_DIR, str(image.id) + "_" + image.get_process_date_short_str() + ".mat")
+            featureFile = os.path.join(FEATURES_DIR, str(image.id) + "_" + image.get_process_date_short_str() + ".dat")
+            if not(os.path.isfile(preprocessedImageFile)) or not(os.path.isfile(featureFile)):
+                # i.after_height_cm_change() # this resets the image
+                img_counter += 1
+        print this_source.name + "has " + str(img_counter) + " images with bad status"
+
+
+
+
